ac6101bab89dc0dbcec1dec0b5505e0d905ae3ae
Rseifert <rseifert.phone@gmail.com>
2013-03-17 00:56:47 -0400

    change the publish bat
    It will now build both mods with version #, built #, etc, etc

--- publish.bat
+++ publish.bat
@@ -7,7 +7,9 @@ set SERVER=
 set USERNAME=
 set PASSWORD=
 
-set /p MODVERSION=<modversion.txt
+set /p CCVERSION=<ccversion.txt
+set /p IEVERSION=<ieversion.txt
+set /p MCVERSION=<mcversion.txt
 set /p CurrentBuild=<buildnumber.txt
 set /a BUILD_NUMBER=%CurrentBuild%+1
 echo %BUILD_NUMBER% >buildnumber.txt
@@ -16,7 +18,8 @@ if %PROMOTION%==* (
 	echo %MODVERSION% >recommendedversion.txt
 )
 
-set FILE_NAME=IllustriousElements_#%BUILD_NUMBER%.jar
+set FILE_ELRATH=[%MCVERSION%]IllustriousElements_v%IEVERSION%#%BUILD_NUMBER%.jar
+set FILE_CC=[%MCVERSION%]ComeCloser_v%CCVERSION%#%BUILD_NUMBER%.jar
 
 echo Starting to build %FILE_NAME%
 
@@ -26,10 +29,11 @@ runtime\bin\python\python_mcp runtime\reobfuscate.py %*
 
 ::ZIP-UP
 cd reobf\minecraft\
-"..\..\..\7za.exe" a "..\..\builds\%FILE_NAME%" "elrath18"
-"..\..\..\7za.exe" a "..\..\builds\%FILE_NAME%" "universalelectricity"
+"..\..\..\7za.exe" a "..\..\builds\IllustriousElements\%FILE_ELRATH%" "elrath18"
+"..\..\..\7za.exe" a "..\..\builds\IllustriousElements\%FILE_ELRATH%" "universalelectricity"
+"..\..\..\7za.exe" a "..\..\builds\ComeCloser\%FILE_CC%" "dark/comecloser"
 cd ..\..\
 
-echo Done building %FILE_NAME%
+echo Done building Files
 
 pause
--- src/minecraft/elrath18/decor/IllustriousElements.java
+++ src/minecraft/elrath18/decor/IllustriousElements.java
@@ -40,7 +40,7 @@ import elrath18.decor.items.ItemStainGlass;
 public class IllustriousElements
 {
 	// TODO Change in Version Release
-	public static final String VERSION = "0.1.2";
+	public static final String VERSION = "0.2.1";
 
 	// Constants
 	public static final String NAME = "Illustrious_Elements";
