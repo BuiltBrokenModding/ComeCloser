ee2b8e6c7d97367a1b9decf206a34e3d0b8a34b7
Robert Seifert <rseifert.phone@gmail.com>
2013-06-17 06:10:36 -0400

    Fixed color dust item textures

--- src/minecraft/dark/illus/items/ItemColored.java
+++ src/minecraft/dark/illus/items/ItemColored.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityEggInfo;
+import net.minecraft.entity.EntityList;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
@@ -13,8 +15,9 @@ import dark.illus.IllustriousElements;
 
 public class ItemColored extends Item
 {
+	private Icon theIcon;
+
 	@SideOnly(Side.CLIENT)
-	private Icon[] icons;
 
 	public ItemColored(int par1, String name)
 	{
@@ -25,22 +28,30 @@ public class ItemColored extends Item
 		this.setUnlocalizedName(name);
 	}
 
-	@Override
-	public Icon getIconFromDamage(int par1)
+	@SideOnly(Side.CLIENT)
+	public int getColorFromItemStack(ItemStack par1ItemStack, int par2)
 	{
-		return icons[~par1 & 15];
+		return IllustriousElements.dyeColors[par1ItemStack.getItemDamage() % 16].getRGB();
 	}
+
+	@SideOnly(Side.CLIENT)
+	public boolean requiresMultipleRenderPasses()
+	{
+		return true;
+	}
+
+	/* @SideOnly(Side.CLIENT)	
+	public Icon getIconFromDamageForRenderPass(int par1, int par2)
+	{
+		return par2 > 0 ? this.theIcon : super.getIconFromDamageForRenderPass(par1, par2);
+	} */
 
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerIcons(IconRegister par1IconRegister)
 	{
-		this.icons = new Icon[IllustriousElements.dyeColorNames.length];
-
-		for (int i = 0; i < IllustriousElements.dyeColorNames.length; ++i)
-		{
-			this.icons[i] = par1IconRegister.registerIcon(IllustriousElements.dyeColorNames[i] + this.getUnlocalizedName().replace("item.",""));
-		}
+		this.itemIcon = par1IconRegister.registerIcon(IllustriousElements.TEXTURE_NAME_PREFIX+"dust");
+		this.theIcon = par1IconRegister.registerIcon("glowingPowder_overlay");
 	}
 
 	@Override
