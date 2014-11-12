8864a7f7a92dd083783a0f3fba8bb918cd3c92eb
Parker Young <Parker8283@users.noreply.github.com>
2014-11-12 01:47:41 -0500

    Initial 1.7.10 update. Untested.

--- src/main/java/dark/comecloser/CommonProxy.java
+++ src/main/java/dark/comecloser/CommonProxy.java
@@ -1,25 +1,19 @@
 package dark.comecloser;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.network.INetworkManager;
-import net.minecraft.network.NetLoginHandler;
-import net.minecraft.network.packet.NetHandler;
-import net.minecraft.network.packet.Packet1Login;
-import net.minecraft.network.packet.Packet250CustomPayload;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ChatMessageComponent;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.network.IConnectionHandler;
-import cpw.mods.fml.common.network.IPacketHandler;
-import cpw.mods.fml.common.network.Player;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.util.ChatComponentText;
 
-public class CommonProxy implements IPacketHandler, IConnectionHandler
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
+import cpw.mods.fml.common.network.internal.FMLProxyPacket;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+public class CommonProxy
 {
-
 	public void changeTagRange(float max, float min)
 	{
 		ComeCloser.sneakRange = min;
@@ -27,84 +21,29 @@ public class CommonProxy implements IPacketHandler, IConnectionHandler
 		this.sendPacket(null,max, min);
 	}
 
-	public void sendPacket(Player player, float max, float min)
+	public void sendPacket(EntityPlayer player, float max, float min)
 	{
-		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-		DataOutputStream data = new DataOutputStream(bytes);
-		try
-		{
-			data.writeFloat(min);
-			data.writeFloat(max);
-
-			Packet250CustomPayload packet = new Packet250CustomPayload();
-			packet.channel = ComeCloser.CHANNEL;
-			packet.data = bytes.toByteArray();
-			packet.length = packet.data.length;
-			if(player instanceof EntityPlayer)
-			{
-				//TODO setup packet to send only to the player and not everyone
-			}
-			if (FMLCommonHandler.instance().getMinecraftServerInstance() != null)
-			{
-				FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().sendPacketToAllPlayers(packet);
-			}
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+		ByteBuf buf = Unpooled.buffer();
+		buf.writeFloat(min);
+		buf.writeFloat(max);
+		FMLProxyPacket packet = new FMLProxyPacket(buf, ComeCloser.CHANNEL);
+		ComeCloser.packetHandler.sendToAll(packet); //TODO setup packet to send only to the player and not everyone
 	}
 
-	@Override
-	public void playerLoggedIn(Player p, NetHandler netHandler, INetworkManager manager)
+	@SubscribeEvent
+	public void playerLoggedIn(FMLNetworkEvent.ServerConnectionFromClientEvent event)
 	{
-		EntityPlayer player = netHandler.getPlayer();
+		EntityPlayerMP player = ((NetHandlerPlayServer)event.handler).playerEntity;
 		if (player != null && !player.worldObj.isRemote)
 		{
-			player.sendChatToPlayer(ChatMessageComponent.createFromText("ComeCloser>>TagRange:" + ComeCloser.sneakRange + "<->" + ComeCloser.standingRange));
+			player.addChatMessage(new ChatComponentText("ComeCloser>>TagRange:" + ComeCloser.sneakRange + "<->" + ComeCloser.standingRange));
 		}
-		this.sendPacket(p, ComeCloser.standingRange, ComeCloser.sneakRange);
+		this.sendPacket(player, ComeCloser.standingRange, ComeCloser.sneakRange);
 	}
 
-	@Override
-	public String connectionReceived(NetLoginHandler netHandler, INetworkManager manager)
+	@SubscribeEvent
+	public void clientLoggedIn(FMLNetworkEvent.ClientConnectedToServerEvent event)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		this.sendPacket(FMLClientHandler.instance().getClientPlayerEntity(), ComeCloser.standingRange, ComeCloser.sneakRange);
 	}
-
-	@Override
-	public void connectionOpened(NetHandler netClientHandler, String server, int port, INetworkManager manager)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void connectionOpened(NetHandler netClientHandler, MinecraftServer server, INetworkManager manager)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void connectionClosed(INetworkManager manager)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void clientLoggedIn(NetHandler clientHandler, INetworkManager manager, Packet1Login login)
-	{
-		this.sendPacket((Player) clientHandler.getPlayer(),ComeCloser.standingRange, ComeCloser.sneakRange);
-
-	}
-
-	@Override
-	public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player)
-	{
-		/* Server side can't receive packets */
-	}
-
 }
